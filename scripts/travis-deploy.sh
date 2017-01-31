82de5ecbbf7dcabc8de4f0b6f7698624f804cb4c
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-01-31 10:09:17 -0800

    gradle --stacktrace for bintray upload

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -15,4 +15,4 @@ set -ev
 ./gradlew build 
 
 # Upload the update size
-./gradlew build :jpl.imce.oml.specification.repository:bintrayUpload --info
+./gradlew build :jpl.imce.oml.specification.repository:bintrayUpload --info --stacktrace
