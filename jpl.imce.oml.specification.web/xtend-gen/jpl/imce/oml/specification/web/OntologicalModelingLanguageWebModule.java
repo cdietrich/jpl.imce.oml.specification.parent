c58f2e44e8cd4c56b35f61f2e14f59c922cd6e52
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-02-09 06:37:43 -0800

    doc readme

--- jpl.imce.oml.specification.web/xtend-gen/jpl/imce/oml/specification/web/OntologicalModelingLanguageWebModule.java
+++ jpl.imce.oml.specification.web/xtend-gen/jpl/imce/oml/specification/web/OntologicalModelingLanguageWebModule.java
@@ -14,7 +14,7 @@ import org.eclipse.xtend.lib.annotations.FinalFieldsConstructor;
 @FinalFieldsConstructor
 @SuppressWarnings("all")
 public class OntologicalModelingLanguageWebModule extends AbstractOntologicalModelingLanguageWebModule {
-  public OntologicalModelingLanguageWebModule(final Provider<ExecutorService> arg0) {
-    super(arg0);
+  public OntologicalModelingLanguageWebModule(final Provider<ExecutorService> executorServiceProvider) {
+    super(executorServiceProvider);
   }
 }
